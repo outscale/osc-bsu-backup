6bdf0a3d1472d298354caaaa77d9d01093e6a0aa
Rémi Jouannet <remi.jouannet@outscale.com>
2020-04-15 23:13:58 +0200

    split each assert in a method

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -4,12 +4,21 @@ import logging
 
 
 class TestUtilsMethods(unittest.TestCase):
-    def test_setup_logging(self):
+    def setUp(self):
+        logging.disable(logging.CRITICAL)
+
+    def tearDown(self):
+        logging.disable(logging.NOTSET)
+
+    def test_setup_logging1(self):
         self.assertIsInstance(utils.setup_logging(), logging.RootLogger)
+    def test_setup_logging2(self):
         self.assertIsInstance(utils.setup_logging("aaa"), logging.Logger)
+    def test_setup_logging3(self):
         self.assertEqual(
             utils.setup_logging("aaa", logging.DEBUG).getEffectiveLevel(), logging.DEBUG
         )
+    def test_setup_logging4(self):
         self.assertEqual(
             utils.setup_logging("aaa", logging.INFO).getEffectiveLevel(), logging.INFO
         )
